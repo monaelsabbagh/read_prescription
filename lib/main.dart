d652969fe68a061b799cc4fd73ec845eec529b0f
monaelsabbagh <monaelsabbagh02@gmail.com>
2026-02-01 16:13:58 +0200

    initial component

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'Screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  SplashScreen(),
+     // home:  SplashScreen(),
     );
   }
 }
